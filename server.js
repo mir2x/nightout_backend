4f4c4860388c6e43d27ed1db72d39f9a6d94e9d3
Shehabuddin-Tushar <freelancerrtushar@gmail.com>
2024-03-27 11:39:45 +0600

    initial setup

---
+++ server.js
@@ -0,0 +1,21 @@
+const port = process.env.PORT || 5000;
+const dbConnection = require("./config/db");
+const app = require("./app");
+require("dotenv").config();
+
+dbConnection(process.env.DB_URL);
+const server = app.listen(port, "192.168.10.13", () => {
+  console.log("Application running on port", port);
+});
+
+const io = require("socket.io")(server, {
+  pingTimeout: 60000,
+  cors: {
+    origin: "http://localhost:5173",
+  },
+});
+
+// Socket.IO
+io.on("connection", (socket) => {
+  console.log(`Socket ${socket.id} connected`);
+});
