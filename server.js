9fc1daa0ef59b0b7c1afb2978b83e345a7c9dee3
Yeasin Arafat <leoarafat10@gmail.com>
2024-05-30 10:39:04 +0600

    refactor socket controller

--- server.js
+++ server.js
@@ -4,8 +4,8 @@ const app = require("./app");
 require("dotenv").config();
 
 dbConnection(process.env.DB_URL);
-const server = app.listen(port, "192.168.10.45", () => {
-  console.log("Application running on port",`192.168.10.45:`+port);
+const server = app.listen(port, "192.168.10.116", () => {
+  console.log("Application running on port", `192.168.10.116:` + port);
 });
 
 const io = require("socket.io")(server, {
