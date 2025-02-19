90a9248698b8103be6724043ad7b83e2900c0399
Mir Tanim Ahmed <mtahmed_dev@proton.me>
2025-02-19 12:49:24 +0600

    attaching session and file to the request

--- src/express.d.ts
+++ src/express.d.ts
@@ -1,9 +1,13 @@
-import { DecodedUser, DecodedAdmin } from "@schemas/decodedUser";
+import { DecodedUser } from "@schemas/decodedUser";
+import { ClientSession } from "mongoose";
+import fileUpload from "express-fileupload";
 
 declare global {
   namespace Express {
     interface Request {
       user: DecodedUser;
+      session: ClientSession;
+      files?: fileUpload.FileArray | null | undefined;
     }
   }
 }
