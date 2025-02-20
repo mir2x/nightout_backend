2c8895c088ebf043c09ab4b6a91cc485d0637bea
Mir Tanim Ahmed <mtahmed_dev@proton.me>
2025-02-20 17:23:28 +0600

    adding enums for bar

--- src/shared/enums.ts
+++ src/shared/enums.ts
@@ -9,3 +9,28 @@ export enum Gender {
   FEMALE = "FEMALE",
   OTHER = "OTHER"
 }
+
+export enum BarType {
+  Disco = "Disco",
+}
+
+export enum CrowdMeter {
+  High = "High",
+  Medium = "Medium",
+  Low = "Low",
+}
+
+export enum Day {
+  Sun = "Sun",
+  Mon = "Mon",
+  Tue = "Tue",
+  Wed = "Wed",
+  Thu = "Thu",
+  Fri = "Fri",
+  Sat = "Sat",
+}
+
+export enum BarScheduleStatus {
+  Open = "Open",
+  Close = "Close",
+}
