61279999b2958ddd817b29c1f66bd6450fde0e97
Mir Tanim Ahmed <mtahmed_dev@proton.me>
2025-02-26 17:58:41 +0600

    adding update and delete to bar controller

--- src/controllers/barControllers.ts
+++ src/controllers/barControllers.ts
@@ -5,31 +5,27 @@ import createError from "http-errors";
 import Cloudinary from "@shared/cloudinary";
 
 const create = async (req: Request, res: Response, next: NextFunction): Promise<any> => {
-  const { name, barType, waitTime, crowdMeter, reviews, cover, gallery, about }: BarSchema = req.body;
+  const cover = req.body.cover;
+  const gallery = req.body.gallery;
+  const data = JSON.parse(req.body.data);
   const newBar = new Bar({
-    name,
-    barType,
-    waitTime,
-    crowdMeter,
-    reviews,
+    name: data.name,
+    barType: data.barType,
+    waitTime: data.waitTime,
+    crowdMeter: data.crowdMeter,
+    reviews: data.reviews,
     cover,
     gallery,
-    about
+    about: data.about
   });
   const bar = await newBar.save();
   return res.status(StatusCodes.OK).json({success: true, message: "Bar crated successfully.", data: bar});
 }
 
-const update = async (req: Request, res: Response, next: NextFunction): Promise<any> => {
-  const { updatedFields } = req.body;
+const getById = async (req: Request, res: Response, next: NextFunction): Promise<any> => {
   const id = req.params.id;
   const bar = await Bar.findById(id);
-  if(!bar) throw createError(StatusCodes.NOT_FOUND, "Bar not found");
-  if(updatedFields.cover && !bar.cover) {
-    await Cloudinary.remove(bar.cover);
-  }
-  const updatedBar = await Bar.findByIdAndUpdate(id, {$set: updatedFields}, {new : true});
-  return res.status(StatusCodes.OK).json({success: true, message: "Bar updated successfully", data: updatedBar});
+  return res.status(StatusCodes.OK).json({success: true, message: "Bar crated successfully.", data: bar});
 }
 
 const getAllBars = async (req: Request, res: Response, next: NextFunction): Promise<any> => {
@@ -65,10 +61,37 @@ const getAllBars = async (req: Request, res: Response, next: NextFunction): Prom
   });
 };
 
+const update = async (req: Request, res: Response, next: NextFunction): Promise<any> => {
+  const id = req.params.id;
+  const bar = await Bar.findById(id);
+  if(!bar) throw createError(StatusCodes.NOT_FOUND, "Bar not found");
+  let updatedFields: Record<string, any> = {};
+  if(req.body.data) {
+    updatedFields = JSON.parse(req.body.data);
+  }
+  if(req.body.cover && !bar.cover) {
+    await Cloudinary.remove(bar.cover);
+    updatedFields.cover = req.body.cover;
+  }
+  if(!updatedFields) throw createError(StatusCodes.BAD_REQUEST, "Nothing to update");
+  const updatedBar = await Bar.findByIdAndUpdate(id, {$set: updatedFields}, {new : true});
+  return res.status(StatusCodes.OK).json({success: true, message: "Bar updated successfully", data: updatedBar});
+}
+
+const remove = async (req: Request, res: Response, next: NextFunction) : Promise<any> => {
+  const id = req.params.id;
+  const bar = await Bar.findById(id);
+  if(!bar) throw createError(StatusCodes.NOT_FOUND, "Bar not found");
+  await Bar.findByIdAndDelete(id);
+  return res.status(StatusCodes.OK).json({success: true, message: "Bar deleted successfully.", data: bar});
+}
+
 const BarController = {
   create,
+  getById,
   getAllBars,
   update,
+  remove
 }
 
 export default BarController;
