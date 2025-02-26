ca18790b243f63bc7406d6177374af842461b42d
Mir Tanim Ahmed <mtahmed_dev@proton.me>
2025-02-26 18:01:03 +0600

    fixing error and removing await-to-ts

--- src/controllers/privacyControllers.ts
+++ src/controllers/privacyControllers.ts
@@ -6,31 +6,21 @@ import { StatusCodes } from "http-status-codes";
 
 const create = async (req: Request, res: Response, next: NextFunction): Promise<any> => {
   const { text } = req.body;
-  const [error, privacy] = await to(Privacy.create({ text }));
-  if (error) return next(error);
+  const privacy = await Privacy.create({ text });
   return res.status(StatusCodes.CREATED).json({ success: true, message: "Success", data: privacy });
 };
 
 const get = async (req: Request, res: Response, next: NextFunction): Promise<any> => {
-  const [error, privacy] = await to(Privacy.findOne().limit(1));
-  if (error) return next(error);
+  const privacy = await Privacy.findOne().limit(1);
   if (!privacy) return res.status(StatusCodes.OK).json({ success: true, message: "No privacy policy", data: {} });
-  res.status(StatusCodes.OK).json({ success: true, message: "Success", data: privacy });
+  return res.status(StatusCodes.OK).json({ success: true, message: "Success", data: privacy });
 };
 
 const update = async (req: Request, res: Response, next: NextFunction): Promise<any> => {
-  const { text } = req.body;
-
-  if (!text) return next(createError(StatusCodes.BAD_REQUEST, "No text provided"));
-
-  const [error, privacy] = await to(Privacy.findOne());
-  if (error) return next(error);
+  const { data } = req.body;
+  const privacy = await to(Privacy.findOneAndUpdate({$set: data}, {new: true}));
   if (!privacy) return next(createError(StatusCodes.NOT_FOUND, "Privacy policy not found"));
-
-  privacy.text = text;
-  await privacy.save();
-
-  res.status(StatusCodes.OK).json({ success: true, message: "Success", data: privacy });
+  return res.status(StatusCodes.OK).json({ success: true, message: "Success", data: privacy });
 };
 
 const PrivacyController = {
--- src/controllers/tacControllers.ts
+++ src/controllers/tacControllers.ts
@@ -5,30 +5,22 @@ import createError from "http-errors";
 import { StatusCodes } from "http-status-codes";
 
 const create = async (req: Request, res: Response, next: NextFunction): Promise<any> => {
-  const { text } = req.body;
-  const [error, tac] = await to(TaC.create({ text: text }));
-  if (error) return next(error);
-  res.status(StatusCodes.CREATED).json({ success: true, message: "Success", data: tac });
+  const text  = req.body.text;
+  const tac = await TaC.create({ text: text });
+  return res.status(StatusCodes.CREATED).json({ success: true, message: "Success", data: tac });
 };
 
 const get = async (req: Request, res: Response, next: NextFunction): Promise<any> => {
-  const [error, tac] = await to(TaC.findOne().limit(1));
-  if (error) return next(error);
-  if (!tac) res.status(StatusCodes.OK).json({ success: true, message: "No Terms and Conditions", data: {} });
-  res.status(StatusCodes.OK).json({ success: true, message: "Success", data: tac });
+  const tac = await TaC.findOne().limit(1);
+  if (!tac) return res.status(StatusCodes.OK).json({ success: true, message: "No Terms and Conditions", data: {} });
+  return res.status(StatusCodes.OK).json({ success: true, message: "Success", data: tac });
 };
 
 const update = async (req: Request, res: Response, next: NextFunction): Promise<any> => {
-  const { text } = req.body;
-  if (!text) return next(createError(StatusCodes.BAD_REQUEST, "No text provided"));
-
-  const [error, tac] = await to(TaC.findOne());
-  if (error) return next(error);
-  if (!tac) return next(createError(StatusCodes.NOT_FOUND, "Terms and Condition not found"));
-  tac.text = text;
-  await tac.save();
-
-  res.status(StatusCodes.OK).json({ success: true, message: "Success", data: tac });
+  const { data } = req.body;
+  const tac = await TaC.findOneAndUpdate({$set: data}, {new: true});
+  if (!tac) throw createError(StatusCodes.NOT_FOUND, "Terms and Condition not found");
+  return res.status(StatusCodes.OK).json({ success: true, message: "Success", data: tac });
 };
 
 const TaCController = {
